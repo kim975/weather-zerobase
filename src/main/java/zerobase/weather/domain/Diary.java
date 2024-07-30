b0693ab6581445cdd59890e061947856dfc2015c
kim <scilo200@gmail.com>
2024-07-30 22:50:40 +0900

    Diary 기능 개발 및 http 코드 추가

---
+++ src/main/java/zerobase/weather/domain/Diary.java
@@ -0,0 +1,27 @@
+package zerobase.weather.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Diary {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String weather;
+    private String icon;
+    private double temperature;
+    private String text;
+    private LocalDate date;
+}
